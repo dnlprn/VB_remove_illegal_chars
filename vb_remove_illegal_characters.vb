a35a55fe21d1860b10bb79b7a7652ed2950f3263
Daniel Peron <74783835+dnlprn@users.noreply.github.com>
2020-12-08 10:28:05 +0000

    v1.1

---
+++ vb_remove_illegal_characters.vb
@@ -0,0 +1,12 @@
+Private Sub TextBox_TextChanged(sender As Object, e As EventArgs) Handles TextBox.TextChanged
+
+        Dim bad As String = "/\#%&*:<>?|{}~!""" ' Illegal characters for a file name: ~ " # % & * : < > ? / \ { | }
+        Dim index As Integer = TextBox.Text.LastIndexOfAny(bad)
+        Dim cPos As Integer = TextBox.SelectionStart
+
+        If index > -1 Then
+            TextBox.Text = TextBox.Text.Remove(index, 1)
+            TextBox.SelectionStart = index
+        End If
+
+    End Sub
